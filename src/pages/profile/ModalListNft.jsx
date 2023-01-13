7822ac6174ac17e576117b11e94c4eda7d163bf7
Reliance Studio <dk463242@gmail.com>
2023-01-13 16:37:06 +0200

    fixed ModalList

--- src/pages/profile/ModalListNft.jsx
+++ src/pages/profile/ModalListNft.jsx
@@ -34,39 +34,41 @@ const ModalListNft = ({ setActive, account, _id, _status, _price, getMy }) => {
     return (
         <Transition.Root show={open} as={Fragment}>
             <div className={true ? "fixed w-screen h-screen bg-black bg-opacity-80 top-0 left-0 flex items-center justify-center z-50 scale-100" : "fixed w-screen h-screen bg-black bg-opacity-80 top-0 left-0 flex items-center justify-center z-50 scale-0"} onClick={() => setActive(false)}>
-                <div className="rounded-[15px] bg-[#131313] w-[320px] lg:w-[560px] overflow-hidden h-[445px] lg:h-[476px] z-50" onClick={e => e.stopPropagation()}>
-                    <div className="flex flex-row w-full justify-end p-[9px] lg:p-5 activity2">
-                        <Close className="cursor-pointer" onClick={() => setActive(false)} />
-                    </div>
-                    <div className="flex flex-col mt-[20px] text-center">
-                        <p className="text-white text-[36px] lg:text-[46px] font-gilroy font-semibold">Update</p>
-                    </div>
-                    <div className="flex flex-col mt-[35px] px-[15px] lg:px-[30px]">
-                        <select
-                            value={status}
-                            onChange={(e) => { setStatus(e.target.value); }}
-                            className="bg-transparent text-[#828383] text-[17px] font-gilroy font-semibold border-2 border-[#2a2a2a] px-4 h-[56px] rounded-[10px]"
-                        >
-                            <option value="listed" className="bg-black text-[17px] font-gilroy font-semibold">Listed</option>
-                            <option value="delisted" className="bg-black text-[17px] font-gilroy font-semibold">Delisted</option>
-                        </select>
-                    </div>
-                    <div className="flex flex-col px-[15px] lg:px-[30px]">
-                        <input
-                            type="price"
-                            name="price"
-                            id="price"
-                            className="flex w-full mt-[15px] h-[56px] rounded-[10px] text-[#828383] text-[16px] font-gilroy font-semibold placeholder-[#828383] border-2 border-[#2a2a2a] px-[20px] focus:outline-none focus:ring-0 bg-transparent"
-                            placeholder="Price"
-                            aria-describedby="price"
-                            value={price}
-                            onChange={(e) => { setPrice(e.target.value) }}
-                        />
-                    </div>
-                    <div className="flex mt-[30px] justify-center px-4 lg:px-0 pb-[30px]">
-                        <button onClick={onConfirm} className="w-full lg:w-[227px] h-[58px] rounded-[52px] text-black bg-[#beff55] text-[18px] font-gilroy tracking-wide font-semibold text-center lg:ml-[20px] mt-[5px] lg:mt-0">
-                            Confirm
-                        </button>
+                <div className="w-[760px] h-[476px]" onClick={e => e.stopPropagation()}>
+                    <div className="rounded-[15px] bg-[#131313] ml-[100px] mr-[100px] w-[320px] lg:w-[560px] overflow-hidden h-[445px] lg:h-[476px] z-50">
+                        <div className="flex flex-row w-full justify-end p-[9px] lg:p-5 activity2">
+                            <Close className="cursor-pointer" onClick={() => setActive(false)} />
+                        </div>
+                        <div className="flex flex-col mt-[20px] text-center">
+                            <p className="text-white text-[36px] lg:text-[46px] font-gilroy font-semibold">Update</p>
+                        </div>
+                        <div className="flex flex-col mt-[35px] px-[15px] lg:px-[30px]">
+                            <select
+                                value={status}
+                                onChange={(e) => { setStatus(e.target.value); }}
+                                className="bg-transparent text-[#828383] text-[17px] font-gilroy font-semibold border-2 border-[#2a2a2a] px-4 h-[56px] rounded-[10px]"
+                            >
+                                <option value="listed" className="bg-black text-[17px] font-gilroy font-semibold">Listed</option>
+                                <option value="delisted" className="bg-black text-[17px] font-gilroy font-semibold">Delisted</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col px-[15px] lg:px-[30px]">
+                            <input
+                                type="price"
+                                name="price"
+                                id="price"
+                                className="flex w-full mt-[15px] h-[56px] rounded-[10px] text-[#828383] text-[16px] font-gilroy font-semibold placeholder-[#828383] border-2 border-[#2a2a2a] px-[20px] focus:outline-none focus:ring-0 bg-transparent"
+                                placeholder="Price"
+                                aria-describedby="price"
+                                value={price}
+                                onChange={(e) => { setPrice(e.target.value) }}
+                            />
+                        </div>
+                        <div className="flex mt-[30px] justify-center px-4 lg:px-0 pb-[30px]">
+                            <button onClick={onConfirm} className="w-full lg:w-[227px] h-[58px] rounded-[52px] text-black bg-[#beff55] text-[18px] font-gilroy tracking-wide font-semibold text-center lg:ml-[20px] mt-[5px] lg:mt-0">
+                                Confirm
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
